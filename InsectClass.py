0aad329933d956bc20be1b3169a14e9a22fdf958
David Koo <david_koo1@baylor.edu>
2022-02-16 15:44:23 -0600

    Class work 2/16/2022

---
+++ InsectClass.py
@@ -0,0 +1,13 @@
+import random 
+
+class Insect:
+    def __init__(self):
+        self.wings = 4
+        self.legs = 3
+        self.flight = 0
+
+    def flight_length(self):
+        self.flight = random.randint(1,10)
+
+    def get_fly(self):
+        return self.flight_length
